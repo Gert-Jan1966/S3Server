2911bb90b15615b9c62e7b7e95c340461607a349
Gert-Jan1966 <IbmEsql6SucksBigtime>
2016-08-25 22:53:57 +0200

    Verklaring aan Repository interface toegevoegd.

--- S3Server/src/main/java/nl/ou/s3server/repository/SymmetricKeyRepository.java
+++ S3Server/src/main/java/nl/ou/s3server/repository/SymmetricKeyRepository.java
@@ -6,9 +6,13 @@ import nl.ou.s3server.domain.SymmetricKey;
 
 /**
  * Spring Data gebaseerde repository voor MongoDB SymmetricKey documents.<br>
- * 
+ * <br>
  * Voor beschrijving van het Repository pattern, zie: 
- * <a href="http://martinfowler.com/eaaCatalog/repository.html">http://martinfowler.com/eaaCatalog/repository.html</a>.
+ * <a href="http://martinfowler.com/eaaCatalog/repository.html">http://martinfowler.com/eaaCatalog/repository.html</a>.<br>
+ * <br>
+ * Voor de beschrijving van de standaard methods die worden gegenereerd, zie:<br>
+ * <a href="http://docs.spring.io/spring-data/mongodb/docs/1.9.2.RELEASE/api/org/springframework/data/mongodb/repository/MongoRepository.html">
+ * MongoRepository (Spring Data MongoDB 1.9.2)</a>.<br>
  */
 public interface SymmetricKeyRepository extends MongoRepository<SymmetricKey, String> {
 
--- S3Server/src/main/java/nl/ou/s3server/repository/package-info.java
+++ S3Server/src/main/java/nl/ou/s3server/repository/package-info.java
@@ -1,5 +1,5 @@
 /**
  * Deze package bevat de Repositories. <br>
- * Hiervoor wordt gebruik gemaakt van het Spring Data MongoDB framework.
+ * Hierbij wordt gebruik gemaakt van het Spring Data MongoDB framework.
  */
 package nl.ou.s3server.repository;
