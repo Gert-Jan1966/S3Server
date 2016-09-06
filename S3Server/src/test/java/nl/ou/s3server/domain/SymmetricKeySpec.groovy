94bcbbc2348ca0d27a8f7e702f8000d39dc9298a
Gert-Jan1966 <IbmEsql6SucksBigtime>
2016-09-06 23:24:01 +0200

    Unittest op SymmetricKey bijgewerkt.

--- S3Server/src/test/java/nl/ou/s3server/domain/SymmetricKeySpec.groovy
+++ S3Server/src/test/java/nl/ou/s3server/domain/SymmetricKeySpec.groovy
@@ -16,16 +16,20 @@ class SymmetricKeySpec extends Specification {
     @Unroll
     def "Test validatieconstraints op velden SymmetricKey"() {
         when:
-        def symmetricKey = new SymmetricKey(key: key)
+        def symmetricKey = new SymmetricKey(key: key, owner: owner)
        
         then:
         validator.validate(symmetricKey).size() == violations
        
         where:
-        key  || violations
-        null ||     1    
-        ""   ||     1
-        "A"  ||     0
+        key  |    owner    || violations
+        null |    null     ||     2
+        null |   "a@a.a"   ||     1    
+        ""   |   "a@a.a"   ||     1
+        "A"  |   "a@a.a"   ||     0
+        "A"  |    null     ||     1
+        "A"  |     ""      ||     1
+        "A"  |    "a"      ||     1
     }
     
     def "Test transitieve constraints m.b.t. Policies in SymmetricKey"() {
@@ -33,7 +37,7 @@ class SymmetricKeySpec extends Specification {
         def locationPolicy = new S3LocationPolicy()
         def expirationPolicy = new S3ExpirationPolicy()
         def symmetricKey = new SymmetricKey(
-                locationPolicy: locationPolicy, expirationPolicy: expirationPolicy, key: "key")
+                locationPolicy: locationPolicy, expirationPolicy: expirationPolicy, key: "key", owner: "a@a.a")
         
         then: "moeten deze Policies zelf ook ingevulde waarden hebben"
         validator.validate(symmetricKey).size() == 2
